efd68f82ec338f3e13fdb54f401e2a520c883ce8
Max-Morozynskyi <m0r0zin@ukr.net>
2023-02-08 21:36:05 +0200

    refactored to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { Buttons } from './Buttons/Buttons';
 import { Counter } from './Counter/Counter';
@@ -6,54 +6,59 @@ import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 import { Wrapper } from './Wrapper.styled';
 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = { good, neutral, bad };
+
+  const handleIncrement = evt => {
+    const { name } = evt.target;
+    switch (name) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleIncrement = task => {
-    this.setState(prevState => ({
-      [task.target.name]: prevState[task.target.name] + 1,
-    }));
-  };
-
-  totalCount = data => {
-    const terms = Object.values(data);
+  const totalFdbck = () => {
+    const terms = Object.values(options);
     return terms.reduce((sum, term) => sum + term, 0);
   };
 
-  percentCount = (good, sum) => {
-    const value = Math.round((good / sum) * 100);
+  const percentCount = () => {
+    const value = Math.round((good / totalFdbck()) * 100);
     return value ? value : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-    const totalFdbck = this.totalCount(this.state);
-    const percentage = this.percentCount(this.state.good, totalFdbck);
-    return (
-      <Wrapper>
-        <h1>Кафе Expresso собирает отзывы от своих клиентов</h1>
-        <Section title={'Поделитесь своим впечатлением'}>
-          <Buttons options={options} onAddFdbck={this.handleIncrement} />
-        </Section>
-        <Section title={'Результат опроса'}>
-          {totalFdbck === 0 ? (
-            <Notification message="Пожалуйста, оставьте отзыв" />
-          ) : (
-            <Counter
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFdbck}
-              positivePercentage={percentage}
-            />
-          )}
-        </Section>
-      </Wrapper>
-    );
-  }
-}
+  return (
+    <Wrapper>
+      <h1>Кафе Expresso собирает отзывы от своих клиентов</h1>
+      <Section title={'Поделитесь своим впечатлением'}>
+        <Buttons options={Object.keys(options)} onAddFdbck={handleIncrement} />
+      </Section>
+      <Section title={'Результат опроса'}>
+        {totalFdbck() === 0 ? (
+          <Notification message="Пожалуйста, оставьте отзыв!" />
+        ) : (
+          <Counter
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFdbck()}
+            positivePercentage={percentCount()}
+          />
+        )}
+      </Section>
+    </Wrapper>
+  );
+};
